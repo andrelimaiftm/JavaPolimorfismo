7fef468c9c1bedbe71b37fe1b0b04937d777203e
andrelimaiftm <andrelima@iftm.edu.br>
2022-08-26 08:16:38 -0300

    exemplo aula polimorfismo

---
+++ src/Principal.java
@@ -0,0 +1,9 @@
+import java.lang.reflect.AnnotatedArrayType;
+
+import superclasse.Animal;
+
+public class Principal {
+    public static void main(String[] args) throws Exception {
+        Animal a = new Animal(50.0, 5, 4);
+    }
+}
